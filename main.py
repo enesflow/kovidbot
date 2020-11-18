5d8a297c42890b9abeaefb7be11b1929c773b44c
EnxGitHub <65719051+EnxGitHub@users.noreply.github.com>
2020-11-18 22:08:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ bot = telebot.TeleBot(TOKEN)
 
 url = "https://covid19.saglik.gov.tr/covid19api?getir=sondurum"
 
-people = [1155586242]
-# , 1221177293
+people = [1155586242, 1221177293]
+
 delay = {18: 30, 19: 15, 20: 7.5, 21: 5, 200: 1000, 400: 1000, 100: 1000}
 delayfor = None
 

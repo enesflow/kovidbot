a0ab77544817b64b13ad2fb1fafd544b784e7478
EnxGitHub <eneskavcakar34@gmail.com>
2020-12-07 19:48:35 +0300

    bug fixes

--- modules/sendCovidTable.js
+++ modules/sendCovidTable.js
@@ -3,8 +3,17 @@ const longMessage = require("./longMessage");
 const mongo = require("./mongo");
 
 function sendCovidTable(data, bot) {
-    mongo.getPeople.forEach((person) => {
-        bot.sendMessage(person['_id'], longMessage.daily(convertData(data), false));
+    mongo.getPeople((people) => {
+        people.forEach((person) => {
+            bot.sendSticker(
+                person["_id"],
+                "CAACAgEAAxkBAAEBqY5fzly7WaIfk3X8BIU32hpYC25MGwACJAgAAuN4BAABeDMQjC5YwUgeBA",
+            );
+            bot.sendMessage(
+                person["_id"],
+                longMessage.daily(convertData(data), false),
+            );
+        });
     });
 }
 

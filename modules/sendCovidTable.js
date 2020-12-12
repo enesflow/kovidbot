8d1e3e06019b8928ec318b0e1e7648f2312a2141
EnxGitHub <eneskavcakar34@gmail.com>
2020-12-12 23:34:31 +0300

    Update sendCovidTable.js

--- modules/sendCovidTable.js
+++ modules/sendCovidTable.js
@@ -6,28 +6,37 @@ const _ = require("lodash");
 
 function sendCovidTable(data, bot) {
     mongo.getAds((ads) => {
-        axios.get("http://localhost:8001/get-secret").then((people) => {
-            people["data"].forEach((person) => {
-                const chatId = person["_id"];
-                Promise.resolve(
-                    bot.sendMessage(
-                        chatId,
-                        longMessage.daily(convertData(data), false),
-                    ),
-                ).then(() => {
-                    if (!person["pro"]) {
-                        const ad = _.sample(ads);
-                        Promise.resolve(
-                            bot.sendPhoto(chatId, ad["image"], {
-                                caption: ad["title"],
-                            }),
-                        ).then(() => {
-                            bot.sendMessage(chatId, ad["message"].join("\n"));
-                        });
-                    }
+        axios
+            .get(
+                process.env.baseURL ||
+                    ("http://localhost:" + process.env.PORT || 8001) +
+                        process.env.GET,
+            )
+            .then((people) => {
+                people["data"].forEach((person) => {
+                    const chatId = person["_id"];
+                    Promise.resolve(
+                        bot.sendMessage(
+                            chatId,
+                            longMessage.daily(convertData(data), false),
+                        ),
+                    ).then(() => {
+                        if (!person["pro"]) {
+                            const ad = _.sample(ads);
+                            Promise.resolve(
+                                bot.sendPhoto(chatId, ad["image"], {
+                                    caption: ad["title"],
+                                }),
+                            ).then(() => {
+                                bot.sendMessage(
+                                    chatId,
+                                    ad["message"].join("\n"),
+                                );
+                            });
+                        }
+                    });
                 });
             });
-        });
     });
 }
 
